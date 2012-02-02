db1bc5959e2f9fbbcf3778fd7933bc66d2eead01
Prashanth Mohan <prashmohan@gmail.com>
2012-02-01 17:53:05 -0800

    Moving static files into the project

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from django.core.handlers.wsgi import WSGIHandler
-
+import os
 import pinax.env
-
+import sys
+sys.path.append('/var/www/Kaelvi')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Kaelvi.settings'
 
 # setup the environment for Django and Pinax
 pinax.env.setup_environ(__file__)
